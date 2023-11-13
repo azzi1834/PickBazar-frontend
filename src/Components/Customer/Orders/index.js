aedf41c910d7ce2a73054fe9e7119d80865a12a9
azzi.azhar <azzi.azhar1834@gmail.com>
2023-11-13 11:08:50 +0500

    index file change

--- src/Components/Customer/Orders/index.js
+++ src/Components/Customer/Orders/index.js
@@ -47,19 +47,7 @@ export default function Orders() {
   return (
     <>
       {fetchedOrders.isFetching ? (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "rgba(255,255,255,0.8)",
-          }}
-        >
+        <div>
           <Spin size="large" />
         </div>
       ) : emptyOrders ? (
